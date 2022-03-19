bde3f56637e424d6eeb95b5ac3483af6e03e445f
Alessandra Avelino <80168478+alessandraavelino@users.noreply.github.com>
2022-03-19 13:01:56 -0300

    Delete Banco-poo-main directory

--- Banco-poo-main/Banco-poo/src/Aplicacao.java
+++
@@ -1,3 +0,0 @@
-public interface Aplicacao {
-    public void calcularRendimneto();
-}
--- Banco-poo-main/out/production/Banco-poo-main/banco.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE conta (
-    agencia INT NOT NULL,
-    numero INT NOT NULL,
-    saldo DOUBLE NOT NULL,
-    cliente VARCHAR(20)
-
-);
-
-select * from conta;
